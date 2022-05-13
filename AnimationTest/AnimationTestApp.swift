662bb4bdf9e3b3cea6cdbf7c7e09a77f1865acd1
Ryu-nakay-from_MBA <70783066+Ryu-nakay@users.noreply.github.com>
2022-05-13 09:47:15 +0900

    Initial Commit

---
+++ AnimationTest/AnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestApp.swift
+//  AnimationTest
+//
+//  Created by 中山龍 on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
